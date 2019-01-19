a379db8a309f324e415ee3f26b72987a477084ca
Johann Deng <dengliang2005@hotmail.com>
2019-01-19 16:08:31 +0800

    Update edit_blog_steps.rb

--- Cucumber/features/step_definitions/edit_blog_steps.rb
+++ Cucumber/features/step_definitions/edit_blog_steps.rb
@@ -5,6 +5,7 @@ require 'capybara'
 Capybara.default_driver = :selenium # :selenium_chrome and :selenium_chrome_headless are also registered
 
 When /I click on Edit button/ do
+       #Click on Edit button, and input title and text of blog, then save this blog
        click_link('Edit')
        fill_in('Title', with: 'Test002')
        fill_in('Text', with: 'This is my test002')
@@ -12,5 +13,6 @@ When /I click on Edit button/ do
 end
  
 Then /existing article should be updated/ do
+	 #Verify blog has been updated successfully
 	 page.has_content?('Article was successfully updated')
 end
